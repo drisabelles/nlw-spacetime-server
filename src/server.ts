c3f849b38e52b2cef3fa57db765e779fe8979930
Isabelle <drsabelles@outlook.com>
2023-05-19 14:29:20 -0300

    feat: database estructures, memories crud and cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
-// ---- http methods: get, post, patch, put and delete
-
-app.get("/users", async () => {
-  const users = await prisma.user.findMany;
-
-  return users;
+app.register(cors, {
+  origin: true,
 });
-
-// ---- api restful
+app.register(memoriesRoutes);
 
 app
   .listen({
